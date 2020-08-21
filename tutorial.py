b59adaa102c37dcb7bdcecd9ff243288e0620c92
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-08-21 11:47:13 -0400

    bug fix: zero out negative fluxes
    - they could be negative due to approximation/rounding errors

--- tutorial.py
+++ tutorial.py
@@ -61,8 +61,10 @@ def solve_kernel_system(L, b, rho=1, tol=np.finfo(np.float64).eps):
 
 def compute_T(L, Q, rho, emiss, tol=np.finfo(np.float64).eps):
     Q, nmul1 = solve_kernel_system(L, Q, rho, tol)
+    Q = np.maximum(0, Q)
     Q *= 1 - rho
     tmp, nmul2 = solve_kernel_system(L, Q, 1, tol)
+    tmp = np.maximum(0, tmp)
     Q = (1 - emiss)*Q + emiss*tmp
     Q /= emiss*sigma
     Q = Q**(1/4)
