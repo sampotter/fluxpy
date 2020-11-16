ce35037fe6db4b1dac00c3477693ecab51dae623
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-11-16 10:48:58 -0500

    use relative error

--- flux/solve.py
+++ flux/solve.py
@@ -1,17 +1,18 @@
 import numpy as np
 
 
-def solve_kernel_system(L, b, rho=1, tol=np.finfo(np.float64).eps):
+def solve_kernel_system(L, b, rho=1, tol=np.finfo(np.float32).eps):
     x = b
     dx = L@(rho*b)
     nmul = 1
-    err = np.linalg.norm(b - x + dx)
+    bnorm = np.linalg.norm(b)
+    err = np.linalg.norm(b - x + dx)/bnorm
     while np.linalg.norm(err) > tol:
         x = b + dx
         dx = L@(rho*b)
         nmul += 1
         prev_err = err
-        err = np.linalg.norm(b - x + dx)
+        err = np.linalg.norm(b - x + dx)/bnorm
         if abs(err - prev_err) < tol:
             break
     return x, nmul
