1697ee6818962932abb138472e7ba70788b43722
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-11-19 13:18:58 -0500

    add CLI args to ingersoll.py, make executable

--- examples/ingersoll.py
+++ examples/ingersoll.py
@@ -1,3 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run the Ingersoll example')
+    parser.add_argument(
+        '-p', type=int, default=6,
+        help='target edge length: h = (2/3)**p')
+    parser.add_argument(
+        '--e0', type=float, default=15,
+        help='sun angle above horizon in [deg.]')
+    parser.add_argument(
+        '--F0', type=float, default=1000,
+        help='solar constant [W/m^2]')
+    parser.add_argument(
+        '--rho', type=float, default=0.3,
+        help='albedo')
+    parser.add_argument(
+        '--emiss', type=float, default=0.99,
+        help='emissivity')
+    parser.add_argument(
+        '--tol', type=float, default=1e-5,
+        help='tolerance used when assembling compressed form factor matrix')
+    parser.add_argument(
+        '--beta', type=float, default=40,
+        help='Ingersoll crater angle measured from vertical [deg.]')
+    parser.add_argument(
+        '--rc', type=float, default=0.8,
+        help='Crater radius [m]')
+    args = parser.parse_args()
+
 import colorcet as cc
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,85 +42,73 @@ from flux.model import compute_steady_state_temp
 from flux.plot import plot_blocks, tripcolor_vector
 from flux.shape import TrimeshShapeModel, get_surface_normals
 
-# Parameters related to the spatial discretization (triangle mesh)
-p = 6
-h = (2/3)**p
+if __name__ == '__main__':
+    h = (2/3)**args.p
+    e0 = np.deg2rad(args.e0)
+    beta = np.deg2rad(args.beta)
 
-# Form factor matrix parameters
-FF_tol = 1e-7
+    hc = HemisphericalCrater(beta, args.rc, e0, args.F0, args.rho, args.emiss)
+    print('- groundtruth temperature in shadow %1.2f K' % (hc.T_gt,))
 
-# Physical parameters related to the problem
-e0 = np.deg2rad(15)
-F0 = 1000
-rho = 0.3
-emiss = 0.99
+    # Create the triangle mesh
+    V, F, parts = hc.make_trimesh(h, return_parts=True)
+    print('- created a Delaunay mesh with %d points and %d faces' % (
+        V.shape[0], F.shape[0]))
 
-# Geometric parameters controlling the shape of the hemispherical crater
-beta = np.deg2rad(40)
-rc = 0.8
+    # Write the mesh to disk as an OBJ file
+    trimesh.Trimesh(V, F).export('ingersoll.obj')
+    print('- saved Wavefront OBJ file to ingersoll.obj')
 
-hc = HemisphericalCrater(beta, rc, e0, F0, rho, emiss)
-print('- groundtruth temperature in shadow %1.2f K' % (hc.T_gt,))
+    # Flip any upside down triangles
+    N = get_surface_normals(V, F)
+    N[N[:, 2] < 0] *= -1
 
-# Create the triangle mesh
-V, F, parts = hc.make_trimesh(h, return_parts=True)
-print('- created a Delaunay mesh with %d points and %d faces' % (
-    V.shape[0], F.shape[0]))
+    # Create a shape model from the triangle mesh (used for raytracing)
+    shape_model = TrimeshShapeModel(V, F, N)
 
-# Write the mesh to disk as an OBJ file
-trimesh.Trimesh(V, F).export('ingersoll.obj')
-print('- saved Wavefront OBJ file to ingersoll.obj')
+    # FF = CompressedFormFactorMatrix.assemble_using_quadtree(
+    #     shape_model, tol=args.tol)
+    FF = CompressedFormFactorMatrix.assemble_using_partition(
+        shape_model, parts, tol=args.tol)
+    print('- assembled compressed form factor matrix (%1.1f Mb)' %
+          (FF.nbytes/1024**2))
 
-# Flip any upside down triangles
-N = get_surface_normals(V, F)
-N[N[:, 2] < 0] *= -1
+    FF.save('ingersoll.bin')
+    print('- saved compressed form factor matrix to disk')
 
-# Create a shape model from the triangle mesh (used for raytracing)
-shape_model = TrimeshShapeModel(V, F, N)
+    fig, ax = plot_blocks(FF._root)
+    fig.savefig('ingersoll_blocks.png')
+    plt.close(fig)
 
-# FF = CompressedFormFactorMatrix.assemble_using_quadtree(
-#     shape_model, tol=FF_tol)
-FF = CompressedFormFactorMatrix.assemble_using_partition(
-    shape_model, parts, tol=FF_tol)
-print('- assembled compressed form factor matrix (tol = %g, %1.1f Mb)' % (
-    FF_tol, FF.nbytes/1024**2))
+    # FF = FormFactorMatrix(shape_model)
 
-FF.save('ingersoll.bin')
-print('- saved compressed form factor matrix to disk')
+    dir_sun = np.array([np.cos(e0), 0, np.sin(e0)])
+    E = shape_model.get_direct_irradiance(args.F0, dir_sun, eps=1e-6)
 
-fig, ax = plot_blocks(FF._root)
-fig.savefig('ingersoll_blocks.png')
-plt.close(fig)
+    fig, ax = tripcolor_vector(V, F, E, cmap=cc.cm.gray)
+    fig.savefig('ingersoll_E.png')
+    plt.close(fig)
+    print('- wrote ingersoll_E.png to disk')
 
-# FF = FormFactorMatrix(shape_model)
+    T = compute_steady_state_temp(FF, E, args.rho, args.emiss)
+    print('- computed T')
 
-dir_sun = np.array([np.cos(e0), 0, np.sin(e0)])
-E = shape_model.get_direct_irradiance(F0, dir_sun, eps=1e-6)
+    fig, ax = tripcolor_vector(V, F, T, cmap=cc.cm.fire)
+    fig.savefig('ingersoll_T.png')
+    plt.close(fig)
+    print('- wrote ingersoll_T.png to disk')
 
-fig, ax = tripcolor_vector(V, F, E, cmap=cc.cm.gray)
-fig.savefig('ingersoll_E.png')
-plt.close(fig)
-print('- wrote ingersoll_E.png to disk')
+    error = T[parts[1]] - hc.T_gt
+    vmax = abs(error).max()
+    vmin = -vmax
+    fig, ax = tripcolor_vector(
+        V, F[parts[1]], error, vmin=vmin, vmax=vmax, cmap=cc.cm.fire)
+    fig.savefig('error.png')
+    plt.close(fig)
+    print('- wrote error.png to disk')
 
-T = compute_steady_state_temp(FF, E, rho, emiss)
-print('- computed T')
+    max_error = abs(error).max()
+    print('- max error: %1.2f K' % (max_error,))
 
-fig, ax = tripcolor_vector(V, F, T, cmap=cc.cm.fire)
-fig.savefig('ingersoll_T.png')
-plt.close(fig)
-print('- wrote ingersoll_T.png to disk')
-
-error = T[parts[1]] - hc.T_gt
-vmax = abs(error).max()
-vmin = -vmax
-fig, ax = tripcolor_vector(
-    V, F[parts[1]], error, vmin=vmin, vmax=vmax, cmap=cc.cm.fire)
-fig.savefig('error.png')
-plt.close(fig)
-print('- wrote error.png to disk')
-
-max_error = abs(error).max()
-print('- max error: %1.2f K' % (max_error,))
-
-rms_error = np.linalg.norm(error)/np.sqrt(error.size)
-print('- RMS error: %1.2f K' % (rms_error,))
+    rms_error = np.linalg.norm(error)/np.sqrt(error.size)
+    print('- RMS error: %1.2f K' % (rms_error,))
