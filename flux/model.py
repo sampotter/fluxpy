6096876e2e027c12c7fb17f7a8f407054389cc36
sberton2 <stefano.bertone@nasa.gov>
2021-01-22 01:36:56 -0500

    allow for parallel processing of time-steps as E columns (radiosity matmul)

--- flux/model.py
+++ flux/model.py
@@ -1,19 +1,26 @@
 import numpy as np
 
-
 from scipy.constants import sigma # Stefan-Boltzmann constant
-
 
 from flux.solve import solve_radiosity
 
-
 def compute_steady_state_temp(FF, E, rho, emiss,
                               clamp=True, tol=np.finfo(np.float64).eps):
-    B = solve_radiosity(FF, E, rho)[0]
-    if clamp:
-        B = np.maximum(0, B)
-    Q = solve_radiosity(FF, FF@((1 - rho)*B))[0]
-    if clamp:
-        Q = np.maximum(0, Q)
-    T = (((1 - rho)*B + emiss*Q)/(emiss*sigma))**0.25
+
+    # solve "visible" section for all time-steps
+    B_arr = solve_radiosity(FF, E, rho)[0]
+    if len(B_arr.shape)==1:
+        B_arr = B_arr[:,np.newaxis]
+
+    # solve IR section for each time step sequentially
+    T = []
+    for B in B_arr.T:
+        if clamp:
+            B = np.maximum(0, B)
+        IR = FF@((1 - rho)*B)
+        Q = solve_radiosity(FF, IR)[0]
+        if clamp:
+            Q = np.maximum(0, Q)
+        T.append((((1 - rho)*B + emiss*Q)/(emiss*sigma))**0.25)
+
     return T
