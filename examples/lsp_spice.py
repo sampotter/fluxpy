22fd6670492fe0346dfe3f46a4cbf73d8cb94886
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-23 23:36:24 -0500

    latest changes

--- examples/lsp_spice.py
+++ examples/lsp_spice.py
@@ -18,9 +18,11 @@ import spiceypy as spice
 
 import flux.compressed_form_factors as cff
 
+from flux.form_factors import get_form_factor_block
 from flux.model import compute_steady_state_temp
 from flux.plot import tripcolor_vector
 from flux.shape import TrimeshShapeModel
+from flux.util import tic, toc
 
 clktol = '10:000'
 
@@ -66,12 +68,66 @@ shape_model = TrimeshShapeModel(V, F, N)
 FF_path = 'lsp_compressed_form_factors.bin'
 FF = cff.CompressedFormFactorMatrix.from_file(FF_path)
 
+FF_gt = get_form_factor_block(shape_model)
+
 # Compute steady state temperature
 
+tic()
 E = shape_model.get_direct_irradiance(F0, sun_dirs)
-for i in range(100):
-    print('frame = %d' % i)
-    T = compute_steady_state_temp(FF, E[:, i], rho, emiss)
-    fig, ax = tripcolor_vector(V, F, T, cmap=cc.cm.rainbow)
-    fig.savefig('lsp_T_%03d.png' % i)
-    plt.close(fig)
+print(f'- got direct irradiances (batched) [{toc()}s]')
+
+tic()
+for i, sun_dir in enumerate(sun_dirs):
+    E[:, i] = shape_model.get_direct_irradiance(F0, sun_dir)
+print(f'- got direct irradiances (regular) [{toc()}s]')
+
+tic()
+T = np.empty_like(E)
+for i, e in enumerate(E.T):
+    T[:, i] = compute_steady_state_temp(FF, e, rho, emiss)
+print(f'- computed steady state temps (compressed FF) [{toc()}s]')
+
+tic()
+vmin, vmax = T.min(), T.max()
+print('saving plots')
+for i, t in enumerate(T.T):
+    print('- frame = %d' % i)
+    plt.figure(figsize=(8, 8))
+    plt.tripcolor(*V[:, :2].T, F, t, vmin=vmin, vmax=vmax, cmap=cc.cm.rainbow)
+    plt.colorbar()
+    plt.savefig('lsp_T_%03d.png' % i)
+    plt.close()
+print(f'made plots in {toc()}s')
+
+tic()
+T_gt = np.empty_like(E)
+for i, e in enumerate(E.T):
+    T_gt[:, i] = compute_steady_state_temp(FF_gt, e, rho, emiss)
+print(f'- computed steady state temps (groundtruth FF) [{toc()}s]')
+
+tic()
+vmin_gt, vmax_gt = T_gt.min(), T_gt.max()
+print('saving plots (groundtruth)')
+for i, t_gt in enumerate(T_gt.T):
+    print('- frame = %d' % i)
+    plt.figure(figsize=(8, 8))
+    plt.tripcolor(*V[:, :2].T, F, t_gt, vmin=vmin_gt, vmax=vmax_gt,
+                  cmap=cc.cm.rainbow)
+    plt.colorbar()
+    plt.savefig('lsp_T_gt_%03d.png' % i)
+    plt.close()
+print(f'made plots in {toc()}s')
+
+tic()
+T_error = T - T_gt
+vmax_error = abs(T_error).max()
+print('saving plots (error)')
+for i, t_error in enumerate(T_error.T):
+    print('- frame = %d' % i)
+    plt.figure(figsize=(8, 8))
+    plt.tripcolor(*V[:, :2].T, F, t_error, vmin=-vmax_error, vmax=vmax_error,
+                  cmap=cc.cm.coolwarm)
+    plt.colorbar()
+    plt.savefig('lsp_T_error_%03d.png' % i)
+    plt.close()
+print(f'made plots in {toc()}s')
