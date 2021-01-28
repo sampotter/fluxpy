62e4d8dfa55e7f73eafe053935f61fa820af02f6
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-28 13:19:54 -0500

    fix compute_steady_state_temp for 1D input

--- flux/model.py
+++ flux/model.py
@@ -6,21 +6,30 @@ from flux.solve import solve_radiosity
 
 def compute_steady_state_temp(FF, E, rho, emiss,
                               clamp=True, tol=np.finfo(np.float64).eps):
-
-    # solve "visible" section for all time-steps
-    B_arr = solve_radiosity(FF, E, rho)[0]
-    if len(B_arr.shape)==1:
-        B_arr = B_arr[:,np.newaxis]
-
-    # solve IR section for each time step sequentially
-    T = []
-    for B in B_arr.T:
+    if E.ndim == 1:
+        B = solve_radiosity(FF, E, rho)[0]
         if clamp:
             B = np.maximum(0, B)
         IR = FF@((1 - rho)*B)
         Q = solve_radiosity(FF, IR)[0]
         if clamp:
             Q = np.maximum(0, Q)
-        T.append((((1 - rho)*B + emiss*Q)/(emiss*sigma))**0.25)
-
-    return T
+        return (((1 - rho)*B + emiss*Q)/(emiss*sigma))**0.25
+    elif E.ndim == 2:
+        # solve "visible" section for all time-steps
+        B_arr = solve_radiosity(FF, E, rho)[0]
+        if len(B_arr.shape)==1:
+            B_arr = B_arr[:,np.newaxis]
+        # solve IR section for each time step sequentially
+        T = []
+        for B in B_arr.T:
+            if clamp:
+                B = np.maximum(0, B)
+            IR = FF@((1 - rho)*B)
+            Q = solve_radiosity(FF, IR)[0]
+            if clamp:
+                Q = np.maximum(0, Q)
+            T.append((((1 - rho)*B + emiss*Q)/(emiss*sigma))**0.25)
+        return np.array(T)
+    else:
+        raise Exception('E should be a vector or a 2D array')
