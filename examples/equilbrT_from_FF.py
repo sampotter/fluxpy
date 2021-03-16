288766eb9c484003ede31c1cf54c81f61b0d859a
Norbert Schörghofer <norbert@psi.edu>
2021-03-16 08:00:10 -1000

    added progs: grd -> FF.bin -> T (#18)
    * added progs: grd -> FF.bin -> T
    * generate_FF_from_grd.py: grd -> FF.bin
    * equilbrT_from_FF.py: FF.bin -> T (with Ingersoll test)
    * ingersoll41.grd: example input
    
    * more input grids

---
+++ examples/equilbrT_from_FF.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+# Calculates equilibrium temperature based on FF.bin
+# Compares with exact solution for bowl-shaped crater
+
+cmap = dict()
+try:
+    import colorcet as cc
+    cmap['jet'] = cc.cm.rainbow
+    cmap['gray'] = cc.cm.gray
+    cmap['fire'] = cc.cm.fire
+except ImportError:
+    print('failed to import colorcet: using matplotlib colormaps')
+    cmap['jet'] = 'jet'
+    cmap['gray'] = 'gray'
+    cmap['fire'] = 'inferno'
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import time
+import json
+
+from flux.compressed_form_factors import CompressedFormFactorMatrix
+from flux.plot import tripcolor_vector
+from flux.model import compute_steady_state_temp
+from flux.solve import solve_radiosity
+from flux.util import tic, toc
+from scipy.constants import Stefan_Boltzmann as sigSB
+
+
+
+    
+def exact_solution_ingersoll(F0, e0, rho, emiss, D2d, P, N, E, dir_sun):
+    # calculate analytical solution for bowl-shaped crater
+    # F0 ... solar constant [W/m^2]
+    # e0 ... elevation of sun above horizontal surface [radians]
+    # rho ... albedo
+    # emiss ... (infrared) emissivity 
+    # D2d ... diameter-to-depth ratio
+    # P ... coordinates of triangle centers
+    # N ... surface normals of length 1
+    # E ... direct solar irradiance
+    # dir_sun ... vector pointing toward sun
+    
+    f = 1 / (1 + 0.25*D2d**2)
+    b = f*(emiss + rho*(1-f)) / (1 - rho*f)
+    T = np.empty(P.shape[0])
+    T[:] = np.nan
+    Eexact = np.empty(P.shape[0])
+    Eexact[:] = np.nan
+
+    # plain around crater
+    k = np.where( P[:,2]==0 )
+    Eexact[k] = F0 * np.sin(e0) 
+    Q = (1-rho) * F0 * np.sin(e0)
+    T[k] = ( Q/(emiss*sigSB) )**0.25 
+    
+    # shadowed portion of crater
+    k = np.where( E==0 )
+    Eexact[k] = 0.
+    Q = (1-rho) * F0 * b*np.sin(e0)
+    T[k] = ( Q/(emiss*sigSB) )**0.25
+    
+    # sunlit portion of crater
+    k = np.where( np.logical_and(E>0,  P[:,2]<0) )
+    e = np.pi/2 - np.arccos( np.dot(N[k], dir_sun) )
+    Eexact[k] = F0 * np.sin(e)
+    Q = (1-rho) * F0 * ( np.sin(e) + b*np.sin(e0) )
+    T[k] = ( Q/(emiss*sigSB) )**0.25
+
+    return T
+    
+
+
+
+if __name__ == '__main__':
+    
+    # load FF.bin previously generated
+    FF = CompressedFormFactorMatrix.from_file('FF.bin')
+    print('- loaded FF.bin')
+    shape_model = FF.shape_model
+    V = FF.shape_model.V
+    F = FF.shape_model.F
+    N = FF.shape_model.N
+    P = FF.shape_model.P
+    print('- Number of vertices',V.shape[0],'Number of facets',F.shape[0])
+    
+    # Define constants used in the simulation:
+    e0 = np.deg2rad(10) # Solar elevation angle
+    F0 = 1365 # Solar constant
+    rho = 0.3
+    emiss = 0.95
+    
+    dir_sun = np.array([0, -np.cos(e0), np.sin(e0)]) # Direction of sun
+    
+    # Compute the direct irradiance and find the elements which are in shadow.
+    tic()
+    E = shape_model.get_direct_irradiance(F0, dir_sun)
+    t_E = toc()
+    I_shadow = E == 0
+    print('Number of elements in E =',E.shape[0])
+    
+    # Make plot of direct irradiance
+    fig, ax = tripcolor_vector(V, F, E, cmap=cmap['gray'])
+    fig.savefig('E.png')
+    plt.close(fig)
+    print('wrote E.png')
+
+    tic()
+    B, niter_B = solve_radiosity(FF, E, rho=rho)
+    t_B = toc()
+    print('- computed radiosity [%1.2f s]' % (t_B,))
+
+    fig, ax = tripcolor_vector(V, F, B, cmap=cc.cm.gray)
+    fig.savefig('B.png')
+    plt.close(fig)
+    print('- wrote B.png')
+
+    tic()
+    T = compute_steady_state_temp(FF, E, rho, emiss)
+    t_T = toc()
+    print('- computed T [%1.2f s]' % (t_T,))
+
+    fig, ax = tripcolor_vector(V, F, T, cmap=cc.cm.fire)
+    fig.savefig('T.png')
+    plt.close(fig)
+    print('- wrote T.png')
+
+    stats = {
+        'e0_deg': np.rad2deg(e0),
+        'F0': F0,
+        'rho': rho,
+        'emiss': emiss,
+        'num_faces': F.shape[0],
+        't_E': float(t_E),
+        't_B': float(t_B),
+        't_T': float(t_T),
+        'niter_B': niter_B
+    }
+    with open('stats.json', 'w') as f:
+        json.dump(stats, f)
+    print('- wrote stats.json')
+    
+    # IF the input was a bowl-shape crater, the solution can be compared with
+    # the exact analytical solution. Otherwise
+    # exit()
+    
+    # calculate analytical solution
+    Texact = exact_solution_ingersoll( F0, e0, rho, emiss, 5., \
+                                              P, N, E, dir_sun)
+    
+    fig, ax = tripcolor_vector(V, F, Texact, cmap=cc.cm.fire)
+    fig.savefig('Texact.png')
+    plt.close(fig)
+    print('- wrote Texact.png')
+
+    err = T - Texact
+    max_error = np.linalg.norm(err, np.inf)
+    abs_error = np.linalg.norm(err, 1) / err.size
+    rms_error = np.linalg.norm(err, 2) / err.size
+
+    vlim = ( np.percentile(err,95) + np.percentile(err,5) )/2
+    fig, ax = tripcolor_vector(V, F, err, vmin=-vlim, vmax=+vlim, cmap='seismic')
+    fig.savefig('error.png')
+    plt.close(fig)
+    print('- wrote error.png')
+    
+    print('num_faces',F.shape[0],
+        'max_error',float(max_error),
+        'abs_error',float(abs_error),
+        'rms_error',float(rms_error) )
+
+
---
+++ examples/generate_FF_from_grd.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+# generates topography-agnostic mesh and the form factor matrix
+# processes grd file in native resolution (no interpolation)
+
+
+cmap = dict()
+try:
+    import colorcet as cc
+    cmap['jet'] = cc.cm.rainbow
+    cmap['gray'] = cc.cm.gray
+    cmap['fire'] = cc.cm.fire
+except ImportError:
+    print('failed to import colorcet: using matplotlib colormaps')
+    cmap['jet'] = 'jet'
+    cmap['gray'] = 'gray'
+    cmap['fire'] = 'inferno'
+
+
+import matplotlib.pyplot as plt
+import meshio
+import netCDF4
+import numpy as np
+import os
+import scipy.interpolate
+import time
+
+from flux.compressed_form_factors import CompressedFormFactorMatrix
+from flux.plot import plot_blocks, tripcolor_vector
+from flux.shape import TrimeshShapeModel
+
+
+if __name__ == '__main__':
+    
+    # Load a DEM stored as a netCDF4 file, and pull out the coordinate data.
+    path = os.path.join('.', 'ingersoll81.grd')
+    rootgrp = netCDF4.Dataset(path)
+    X = np.array(rootgrp.variables['x'])
+    Y = np.array(rootgrp.variables['y'])
+    Z = np.array(rootgrp.variables['z'])
+    x0, x1 = rootgrp.variables['x'].actual_range
+    y0, y1 = rootgrp.variables['y'].actual_range
+    nx = rootgrp.dimensions['x'].size
+    ny = rootgrp.dimensions['y'].size
+    print('x_min:',x0, 'x_max:',x1, 'x_inc:',(x1-x0)/(nx-1), 'n_columns:',nx)
+    print('y_min:',y0, 'y_max:',y1, 'y_inc:',(y1-y0)/(ny-1), 'n_rows:',ny)
+    del rootgrp, x0, x1, y0, y1
+    print('loaded',path)
+
+    # Create function z = z(x, y) that linearly interpolates DEM data
+    z = scipy.interpolate.interp2d(X, Y, Z)
+
+    # generate (x,y) from grd file
+    X_mesh, Y_mesh = np.meshgrid(X,Y)
+    points_mesh = np.array([X_mesh.flatten(), Y_mesh.flatten()]).T
+    # with native values of (x,y), Delaunay triangulates without interpolation
+    delaunay = scipy.spatial.Delaunay(points_mesh)
+    V, F = delaunay.points, delaunay.simplices
+    
+    V = np.row_stack([V.T, np.array([z(*v)[0] for v in V])]).T
+    num_faces = F.shape[0]
+    print('created mesh with %d triangles' % num_faces)
+
+    # Let's use another Python library (meshio) to save the triangle
+    # mesh as an OBJ file.
+    points = V
+    cells = [('triangle', F)]
+    mesh = meshio.Mesh(points, cells)
+    mesh_path = 'mesh%d.obj' % num_faces
+    mesh.write(mesh_path)
+    print('wrote %s to disk' % mesh_path)
+
+    # Make plot of topography
+    fig, ax = tripcolor_vector(V, F, V[:,2], cmap=cmap['jet'])
+    fig.savefig('topo2.png')
+    plt.close(fig)
+    print('wrote topo2.png')
+    
+    # Since Embree runs in single precision, there's no reason to use
+    # double precision here.
+    V = V.astype(np.float32)
+
+    # Create a triangle mesh shape model using the vertices (V) and
+    # face indices (F).
+    shape_model = TrimeshShapeModel(V, F)
+
+    # Build the compressed form factor matrix. All of the code related
+    # to this can be found in the "form_factors.py" file in this
+    # directory.
+    t0 = time.perf_counter()
+    FF = CompressedFormFactorMatrix.assemble_using_quadtree(
+        shape_model, tol=np.finfo(np.float32).resolution)
+    print('assembled form factor matrix in %f sec (%1.2f MB)' %
+          (time.perf_counter() - t0, FF.nbytes/(1024**2),))
+    del t0
+
+    # Python makes it very easy to serialize object hierarchies and
+    # write them to disk as binary files. We do that here to save the
+    # compressed form factor matrix. We can reload it later if we
+    # want, without having to first compute it (or load an OBJ file,
+    # or set up Embree, or any of that stuff).
+    FF.save('FF.bin')
+    print('saved compressed form factor matrix to FF.bin')
+
+    # Make plot of blocks in form factor matrix
+    fig, ax = plot_blocks(FF._root)
+    fig.savefig('blocks.png')
+    plt.close(fig)
+    print('wrote blocks.png')
+    
+    # exit()   # convenient test below
+    
+    # Define constants used in the simulation:
+    e0 = np.deg2rad(10) # Solar elevation angle
+    F0 = 1365 # Solar constant
+    dir_sun = np.array([0, -np.cos(e0), np.sin(e0)]) # Direction of sun
+    
+    # Compute the direct irradiance and find the elements which are in shadow.
+    E = shape_model.get_direct_irradiance(F0, dir_sun)
+    I_shadow = E == 0
+
+    # Make plot of direct irradiance
+    fig, ax = tripcolor_vector(V, F, E, cmap='gray')
+    fig.savefig('E.png')
+    plt.close(fig)
+    print('wrote E.png')
+
