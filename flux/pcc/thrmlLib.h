fc69e67f1abf68368ef790dc3be4ef95d0b11cdc
sberton2 <stefano.bertone@nasa.gov>
2021-03-20 00:40:52 -0400

    added support for conductionT and test against analytical formulation

--- flux/pcc/thrmlLib.h
+++ flux/pcc/thrmlLib.h
@@ -3,6 +3,10 @@
 void conductionQ(int, double [], double, double, double,
 		double [], double [], double [], double, double, double *);
 
+void conductionT(int nz, double z[], double dt, double T[], double Tsurf,
+		 double Tsurfp1, double ti[], double rhoc[], double Fgeotherm,
+		 double *Fsurf);
+
 double flux_noatm(double R, double decl, double latitude, double HA,
 		  double surfaceSlope, double azFac);
 
