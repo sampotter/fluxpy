9204324049955c65396191d980841e7836fd8ece
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-10-20 14:15:46 -0400

    clean up

--- flux/compressed_form_factors.py
+++ flux/compressed_form_factors.py
@@ -237,10 +237,6 @@ class FormFactorBlockMatrix(CompressedFormFactorBlock,
         nnz, shape = spmat.nnz, spmat.shape
         size = np.product(shape)
         sparsity = nnz/size
-        # print(size)
-        # exit()
-
-        # print(nnz, shape, size, sparsity, self._min_size, self._sparsity_threshold)
 
         if shape[0] == 0 and shape[1] == 0:
             return self.root.make_null_block()
@@ -249,28 +245,18 @@ class FormFactorBlockMatrix(CompressedFormFactorBlock,
             return self.root.make_zero_block(shape)
 
         if size < self._min_size:
-            # if not is_diag:
-            #     # print("make svd", size, sparsity)
-            #     return self.make_compressed_sparse_block(spmat)
-            # else:
             if sparsity < self._sparsity_threshold:
-                # print("make sparse", size, sparsity)
                 return self.root.make_sparse_block(spmat, fmt='csr')
             else:
-                # print("make dense", size, sparsity)
                 return self.root.make_dense_block(spmat.toarray())
         else:
-            # print("Entering here since", size, self._min_size)
             if not is_diag:
-                # print("make svd", size, sparsity)
                 block = self.make_compressed_sparse_block(spmat)
             else:
                 block = self.make_child_block(shape_model, spmat, I, J)
                 if block.is_dense():
-                    # print("make dense child", size, sparsity)
                     block = self.root.make_dense_block(block.toarray())
                 elif block.is_sparse():
-                    # print("make sparse child", size, sparsity)
                     block = self.root.make_sparse_block(block.tocsr())
             return block
 
