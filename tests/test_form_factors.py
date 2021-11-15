e77c0e00f688d16b6046c2637888ea1137dc3b70
Samuel F. Potter <sfp@cims.nyu.edu>
2021-11-14 20:47:09 -0500

    add start of test_form_factors.py

---
+++ tests/test_form_factors.py
@@ -0,0 +1,32 @@
+import itertools as it
+import numpy as np
+import unittest
+
+from pathlib import Path
+
+from flux.form_factors import get_form_factor_matrix
+from flux.shape import TrimeshShapeModel
+
+class FormFactorsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.data_path = Path(__file__).parent.absolute()/'data'
+
+        np.seterr('raise')
+
+    def test_get_form_factor_matrix_icosa_sphere_5(self):
+        npz_file = np.load(self.data_path/'icosa_sphere_5.npz')
+        shape_model = TrimeshShapeModel(npz_file['V'], npz_file['F'])
+
+        outward = (shape_model.P*shape_model.N).sum(1) > 0
+        shape_model.N[outward] *= -1
+
+        FF = get_form_factor_matrix(shape_model)
+
+        for i, j in it.product(range(shape_model.num_faces), repeat=2):
+            if i == j:
+                self.assertEqual(FF[i, j], 0, f'i = {i}, j = {j}')
+            else:
+                self.assertNotEqual(FF[i, j], 0, f'i = {i}, j = {j}')
+
+if __name__ == '__main__':
+    unittest.main()
