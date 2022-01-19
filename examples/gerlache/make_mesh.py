1315a01f83cff0a273298fdd648eb5f417678844
sberton2 <stefano.bertone@umbc.edu>
2022-01-19 02:18:16 -0500

    gerlache example create/convert mesh

--- examples/gerlache/make_mesh.py
+++ examples/gerlache/make_mesh.py
@@ -1,12 +1,22 @@
 import numpy as np
 import sys
+import pyvista as pv
 
-# Load the DEM as a memory-mapped file
-dem_npy_path = sys.argv[1]
-dem = np.lib.format.open_memmap(dem_npy_path)
+from raster2mesh.mesh_manip import pcd2mesh
+from raster2mesh.raster_roi import roi2pcd
 
-# TODO: convert stereographic to Cartesian
+raster_path = sys.argv[1]
+mesh_resolution = int(sys.argv[2])
+roi_coords = [int(x) for x in list(sys.argv[3].split(','))]
 
-# TODO: use meshpy to create a Delaunay mesh of a specific region
+mesh_path = raster_path[:-4] + '.obj'
 
-# TODO: save mesh to disk
+# open raster and select region to mesh-ify
+# generate mesh of selected region
+pcd = roi2pcd(raster_in=raster_path, raster_out="roi.tif", roi=roi_coords)
+pcd2mesh(pcd, mesh_out=mesh_path, resolution=mesh_resolution)
+
+# read and visualize resulting mesh
+grid = pv.read(mesh_path)
+grid.plot(show_scalar_bar=True, show_axes=True)
+
--- examples/gerlache/run_example.sh
+++ examples/gerlache/run_example.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
-DEM_NAME="ldem_87s_5mpp"
+DEM_NAME="ldem_87s_50mpp"
 DEM_GEOTIFF_PATH="$DEM_NAME.tif"
-DEM_NPY_PATH="$DEM_NAME.npy"
-DEM_URL="https://pgda.gsfc.nasa.gov/data/LOLA_5mpp/87S/ldem_87s_5mpp.tif"
+DEM_OBJ_PATH="$DEM_NAME.obj"
+#DEM_URL="https://pgda.gsfc.nasa.gov/data/LOLA_5mpp/87S/ldem_87s_5mpp.tif"
+DEM_URL="./ldem_87s_50mpp.tif"
 
-if [ ! -f $DEM_NPY_PATH ]; then
-	echo "Didn't find Gerlache DEM. Downloading from PGDA..."
-	wget $DEM_URL
+if [ ! -f $DEM_OBJ_PATH ]; then
+  # I loaded here the 50mpp version for the moment
+#  echo "Didn't find Gerlache DEM. Downloading from PGDA..."
+#  wget -O $DEM_GEOTIFF_PATH $DEM_URL
 
-	echo "Converting from GeoTIFF to .npy..."
-	./convert_geotiff_to_npy.py $DEM_GEOTIFF_PATH
+  echo "Converting from GeoTIFF to .obj..." # one shouldn't use .obj format, it's deprecated
+  python make_mesh.py $DEM_GEOTIFF_PATH 250 -66536,-21239,-22038,32281 # I pre-selected a reasonable roi. Remove last arg to manually select on plot
 
-	echo "Deleting GeoTIFF file."
-	rm $DEM_GEOTIFF_PATH
+  echo "Deleting GeoTIFF file."
+#  rm $DEM_GEOTIFF_PATH
 fi
+
+echo "Convert mesh to cartesian..."
+python unproject_mesh.py $DEM_OBJ_PATH
+
+# compute FF
+
+# get Sun position from SPICE and illuminate
