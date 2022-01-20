f50a2325ff32359c5cf269d2af0d2a7cd3a77523
Samuel F. Potter <sfp@cims.nyu.edu>
2022-01-20 12:56:56 -0500

    oops that made no sense

--- examples/gerlache/dem_error.py
+++ examples/gerlache/dem_error.py
@@ -1,3 +1,4 @@
+import colorcet as cc
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -5,25 +6,26 @@ plt.ion()
 
 dem = np.lib.format.open_memmap(npy_path) # already in meters
 
-h = 5e2 # cm (5mpp DEM)
+Z = dem[10000:10100, 15000:15100] # m originally
 
-Z = dem[10000:10100, 15000:15100] # km originally
+dZ_y_cm = 1e2*np.diff(Z, axis=0)
+dZ_x_cm = 1e2*np.diff(Z, axis=1)
 
-dZ_dx_cm = (1e5*np.diff(Z, axis=0))/h
-dZ_dy_cm = (1e5*np.diff(Z, axis=1))/h
+vmax = max(abs(dZ_y_cm).ravel().max(), abs(dZ_x_cm).ravel().max())
+vmin = -vmax
 
 plt.figure(figsize=(10, 4))
 plt.subplot(1, 3, 1)
-plt.imshow(Z)
+plt.imshow(Z, cmap=cc.cm.rainbow)
 plt.title('DEM [km]')
 plt.colorbar()
 plt.subplot(1, 3, 2)
-plt.imshow(dZ_dx_cm)
-plt.title('$\partial z / \partial x$ [cm]')
+plt.imshow(dZ_x_cm, vmax=vmax, vmin=vmin, cmap=cc.cm.coolwarm)
+plt.title('$\partial z$ in $x$ direction [cm]')
 plt.colorbar()
 plt.subplot(1, 3, 3)
-plt.imshow(dZ_dy_cm)
-plt.title('$\partial z / \partial y$ [cm]')
+plt.imshow(dZ_y_cm, vmax=vmax, vmin=vmin, cmap=cc.cm.coolwarm)
+plt.title('$\partial z$ in $y$ direction [cm]')
 plt.colorbar()
 plt.show()
 
@@ -32,10 +34,10 @@ plt.axvline(x=30, linewidth=1, zorder=1, c='k')
 plt.axvline(x=55, linewidth=1, linestyle='--', zorder=1, c='k')
 plt.axvline(x=-30, linewidth=1, zorder=1, c='k')
 plt.axvline(x=-50, linewidth=1, linestyle='--', zorder=1, c='k')
-plt.hist(dZ_dx_cm.ravel(), zorder=2, histtype='step',
-         label=r'$\partial z / \partial x$ [cm]', bins=129)
-plt.hist(dZ_dy_cm.ravel(), zorder=3, histtype='step',
-         label=r'$\partial z / \partial y$ [cm]', bins=129)
+plt.hist(dZ_x_cm.ravel(), zorder=2, histtype='step',
+         label=r'$\partial z$ in $x$ direction [cm]', bins=129)
+plt.hist(dZ_y_cm.ravel(), zorder=3, histtype='step',
+         label=r'$\partial z$ in $y$ direction [cm]', bins=129)
 plt.legend()
 plt.gca()
 plt.show()
