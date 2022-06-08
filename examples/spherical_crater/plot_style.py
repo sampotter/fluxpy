f82e50b4341d998d5969326f177e44ad82887903
Samuel F. Potter <sfp@cims.nyu.edu>
2022-06-08 11:46:20 -0400

    fix typo

--- examples/spherical_crater/plot_style.py
+++ examples/spherical_crater/plot_style.py
@@ -1,6 +1,6 @@
 linewidth = 1
 dpi = 300
 marker = 'o'
-colors = ['black', 'dodgerblue', 'orangered', 'fuschia']
+colors = ['black', 'dodgerblue', 'orangered', 'fuchsia']
 linestyles = ['-', '--', '-.', ':']
 square_figsize = (9, 9)
