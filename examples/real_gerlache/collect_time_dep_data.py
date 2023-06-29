b0250917af248192bfecf19a99dfd9560932964a
mrepasky3 <mwrepasky@gmail.com>
2023-06-29 09:55:57 -0400

    sync scripts in real_gerlache

--- examples/real_gerlache/collect_time_dep_data.py
+++ examples/real_gerlache/collect_time_dep_data.py
@@ -22,6 +22,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--compression_type', type=str, default="svd",choices=["nmf","snmf","wsnmf",
     "svd","ssvd",
     "rand_svd","rand_ssvd","rand_snmf",
+    "saca","sbrp","rand_sid",
     "true_model"])
 parser.add_argument('--max_inner_area', type=float, default=0.8)
 parser.add_argument('--max_outer_area', type=float, default=3.0)
@@ -127,6 +128,18 @@ elif compression_type == "rand_snmf":
 elif compression_type == "wsnmf":
     FF_dir = "{}_{}_{}_{:.0e}_{:.0e}it_{:.0e}tol_{}k0".format(compression_type if args.nmf_beta_loss==2 else "wsklnmf", max_inner_area_str, max_outer_area_str, args.tol,
         args.nmf_max_iters, args.nmf_tol, args.k0)
+
+elif compression_type == "saca":
+    FF_dir = "{}_{}_{}_{:.0e}_{}k0".format(compression_type, max_inner_area_str, max_outer_area_str, args.tol,
+        args.k0)
+
+elif compression_type == "sbrp":
+    FF_dir = "{}_{}_{}_{:.0e}_{}k0".format(compression_type, max_inner_area_str, max_outer_area_str, args.tol,
+        args.k0)
+
+elif compression_type == "rand_sid":
+    FF_dir = "{}_{}_{}_{:.0e}_{}p_{}q_{}k0".format(compression_type, max_inner_area_str, max_outer_area_str, args.tol,
+        args.p, args.q, args.k0)
 
 
 if not compression_type == "true_model" and args.min_depth != 1:
